2808e7a2a454f264847a063fb52f12dde367c511
anifredrick6-prog <anifredrick6@gmail.com>
2026-07-11 15:32:37 +0100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Landing from './pages/Landing.jsx'
+import Signup from './pages/Signup.jsx'
+import Login from './pages/Login.jsx'
+import Welcome from './pages/Welcome.jsx'
+import ErrorBoundary from './components/ErrorBoundary.jsx'
 
 function App() {
-  return <Landing />
+  return (
+    <ErrorBoundary>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/welcome" element={<Welcome />} />
+        </Routes>
+      </BrowserRouter>
+    </ErrorBoundary>
+  )
 }
 
 export default App
