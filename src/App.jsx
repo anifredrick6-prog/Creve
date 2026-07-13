ac5048bf8ef85402e254c6f5cc64e9fbe285e1f5
anifredrick6-prog <anifredrick6@gmail.com>
2026-07-13 20:58:31 +0100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Welcome from './pages/Welcome.jsx'
 import Marketplace from './pages/Marketplace.jsx'
 import Dashboard from './pages/Dashboard.jsx'
 import VendorProfile from './pages/VendorProfile.jsx'
+import Messages from './pages/Messages.jsx'
+import Conversation from './pages/Conversation.jsx'
 import ErrorBoundary from './components/ErrorBoundary.jsx'
 
 function App() {
@@ -20,6 +22,8 @@ function App() {
           <Route path="/marketplace" element={<Marketplace />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/vendor/:vendorId" element={<VendorProfile />} />
+          <Route path="/messages" element={<Messages />} />
+          <Route path="/messages/:otherId" element={<Conversation />} />
         </Routes>
       </BrowserRouter>
     </ErrorBoundary>
